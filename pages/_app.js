e105cc4951b4a877bb7f83bb2ce8e66101b11ca4
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-04-21 10:18:39 -0300

    feat: fix all problemas with lint

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-import "tailwindcss/tailwind.css";
+import React from 'react';
 
+import 'tailwindcss/tailwind.css';
+
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
 
