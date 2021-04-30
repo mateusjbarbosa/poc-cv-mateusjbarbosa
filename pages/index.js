28b93bbd8afc8912273397cde1a52034be6cd8a8
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-04-30 09:29:17 -0300

    refactor: add footer component on index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import Header from '../src/components/Header';
-import SEO from '../src/components/SEO';
+import Footer from '../src/components/Footer';
 import Main from '../src/components/Main';
+import SEO from '../src/components/SEO';
 
 export default function Home() {
   return (
@@ -13,15 +14,7 @@ export default function Home() {
 
       <Main />
 
-      <footer className="bg-blue-900 flex flex-col items-center p-8">
-        <p className="font-fira text-base text-blue-400 mt-2">
-          Desenvolido por
-          {' '}
-          <a className="text-white" href="http://www.mateusjbarbosa.dev.br" target="_blank" rel="noreferrer">mateusjbarbosa.dev</a>
-          {' '}
-          | 2021
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 }
